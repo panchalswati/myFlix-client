a8d1796e4194df517ad6faf3327051ce95161ebc
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-17 17:13:45 +0200

    added <Container> element

--- src/index.jsx
+++ src/index.jsx
@@ -10,16 +10,11 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <container>
+            <Container>
                 <MainView />
-            </container>
-
+            </Container>
         );
     }
 }
-
-// Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
-
-// Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
