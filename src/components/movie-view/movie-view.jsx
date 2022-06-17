5afaf571e5c715a90374886498b800e959d5a432
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-17 17:10:44 +0200

    added properties for the view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 export class MovieView extends React.Component {
     keypressCallback(event) {
         console.log(event.key);
@@ -32,3 +33,21 @@ export class MovieView extends React.Component {
         );
     }
 }
+
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        }),
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+        })
+    }),
+    onBackClick: PropTypes.func.isRequired
+};
+
+export default MovieView;
