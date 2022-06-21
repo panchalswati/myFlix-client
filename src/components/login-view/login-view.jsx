2111b4f1bf23fa2b2a5d5832a5a791e01095c7e0
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-21 14:06:52 +0200

    added validate() function for user input

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,28 +2,64 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
-import { RegistrationView } from '../registration-view/registration-view';
+import axios from 'axios';
 import './login-view.scss';
 
 export function LoginView(props) {
-    const [username, setUsername] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
+    const [nameErr, setNameErr] = useState('');
+    const [passwordErr, setPasswordErr] = useState('');
 
+    // validate user inputs
+    const validate = () => {
+        let isReq = true;
+        if (!name) {
+            setNameErr('Username Required');
+            isReq = false;
+        } else if (name.length < 2) {
+            setNameErr('Username must be 2 characters long');
+            isReq = false;
+        }
+        if (!password) {
+            setPasswordErr('Password Required');
+            isReq = false;
+        } else if (password.length < 6) {
+            setPassword('Password must be 6 characters long');
+            isReq = false;
+        }
+
+        return isReq;
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        // Send a request to the server for authentication, then call props.onLoggedIn(username)
-        props.onLoggedIn(username);
+        const isReq = validate();
+        if (isReq) {
+            /* Send a request to the server for authentication */
+            axios.post('https://dashboard.heroku.com/apps/myflix-movies-heroku/login', {
+                name: name,
+                password: password
+            })
+                .then(response => {
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(e => {
+                    console.log('no such user')
+                });
+        }
     }
     return (
         <><Form>
-            <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
-                <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            <Form.Group controlId="formName">
+                <Form.Label>name:</Form.Label>
+                <Form.Control type="text" onChange={e => setName(e.target.value)} />
+                {nameErr && <p>{nameErr}</p>}
             </Form.Group>
             <Form.Group controlId="formPassword">
                 <Form.Label>Password:</Form.Label>
                 <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                {passwordErr && <p>{passwordErr}</p>}
             </Form.Group>
 
             <Button variant="primary" type="submit" onClick={handleSubmit}>
@@ -34,7 +70,7 @@ export function LoginView(props) {
 
 LoginView.propTypes = {
     user: PropTypes.shape({
-        username: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
         password: PropTypes.string.isRequired
     }),
     onLoggedIn: PropTypes.func.isRequired,
