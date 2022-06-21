e49121302dfeea1e8e720c3e90c27067106ca02f
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-21 14:15:04 +0200

    added Links to react-router-dom

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './movie-card.scss';
+import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
     render() {
-        const { movie, onMovieClick } = this.props;
+        const { movie } = this.props;
 
         return (
             <Card>
@@ -13,7 +15,9 @@ export class MovieCard extends React.Component {
                 <Card.Body>
                     <Card.Title>{movie.Title}</Card.Title>
                     <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                    <Link to={`/movies/${movies._id}`}>
+                        <Button variant="link">Open</Button>
+                    </Link>
                 </Card.Body>
             </Card>
         );
