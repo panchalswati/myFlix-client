e0b5908a13b7ea7391bb2c2a7086f0ed1f867980
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-22 17:09:16 +0200

    updated label-value according to database

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -5,16 +5,16 @@ import './genre-view.scss';
 
 export class GenreView extends React.Component {
     render() {
-        const { genre, movie, onBackClick } = this.props;
+        const { Genre, onBackClick } = this.props;
         return (
-            <Container>
+            <Container className="genre-view">
                 <Row>
                     <Col className="label">Genre: </Col>
-                    <Col className="value">{genre.Genre.Name}</Col>
+                    <Col className="value">{Genre.Name}</Col>
                 </Row>
                 <Row className="mt-3">
                     <Col className="label">Description: </Col>
-                    <Col className="value">{genre.Genre.Description}</Col>
+                    <Col className="value">{Genre.Description}</Col>
                 </Row>
                 <Button className="d-block mt-3" onClick={() => { onBackClick(null); }} variant="waring">Back</Button>
             </Container>
