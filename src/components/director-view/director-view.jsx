58b85b77a3fa7fb1a2ae5d7e4f5a9c0a32d3b21d
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-22 17:08:25 +0200

    updated the label-value according to database

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,36 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Col, Container, Row, } from 'react-bootstrap';
 import './director-view.scss';
-
+import { Col, Row, Container, Button } from 'react-bootstrap';
 
 export class DirectorView extends React.Component {
     render() {
         const { director, onBackClick } = this.props;
+
         return (
             <Container className="director-view">
                 <Row>
                     <Col className="label">Director: </Col>
-                    <Col className="value">{director.Director.name}</Col>
+                    <Col className="value">{Director.name}</Col>
                 </Row>
                 <Row className="mt-3">
                     <Col className="label">Bio: </Col>
-                    <Col className="value">{director.Director.Bio}</Col>
+                    <Col className="value">{Director.Bio}</Col>
                 </Row>
                 <Row className="mt-3">
                     <Col className="label">Birth: </Col>
-                    <Col classsName="value">{director.Director.Birth}</Col>
+                    <Col className="value">{Director.Birth}</Col>
                 </Row>
-                <Button onClick={() => { onBackClick(null); }} variant="warning">Back</Button>
+                <Button className="d-block mt-3" onClick={() => { onBackClick(null); }} variant="warning">Back</Button>
             </Container>
         )
     }
 }
 DirectorView.propTypes = {
-    director: PropTypes.shape({
-        name: PropTypes.sting.isRequired,
+    Director: PropTypes.shape({
+        name: PropTypes.string.isRequired,
         Bio: PropTypes.string.isRequired,
         Birth: PropTypes.string.isRequired
     }).isRequired
-};
-export default directorView;
+}
