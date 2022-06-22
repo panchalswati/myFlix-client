78a6d015b6f374a98cc89b961b2347f2b458d095
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-22 17:10:37 +0200

    added Container ,Row,Col

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import './login-view.scss';
+import Container from 'react-bootstrap/Container';
 
 export function LoginView(props) {
     const [name, setName] = useState('');
@@ -36,7 +37,7 @@ export function LoginView(props) {
         const isReq = validate();
         if (isReq) {
             /* Send a request to the server for authentication */
-            axios.post('https://dashboard.heroku.com/apps/myflix-movies-heroku/login', {
+            axios.post(' https://myflix-movies-heroku.herokuapp.com/login', {
                 name: name,
                 password: password
             })
@@ -50,21 +51,28 @@ export function LoginView(props) {
         }
     }
     return (
-        <><Form>
-            <Form.Group controlId="formName">
-                <Form.Label>name:</Form.Label>
-                <Form.Control type="text" onChange={e => setName(e.target.value)} />
-                {nameErr && <p>{nameErr}</p>}
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
-                <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-                {passwordErr && <p>{passwordErr}</p>}
-            </Form.Group>
-
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-                Login         </Button>
-        </Form></>
+        <Container id="login-form">
+            <Row className="justify-content-center">
+                <Col sm="10" md="8" lg="6">
+                    <Form>
+                        <Form.Group controlId="formName">
+                            <Form.Label>name:</Form.Label>
+                            <Form.Control type="text" onChange={e => setName(e.target.value)} />
+                            {nameErr && <p>{nameErr}</p>}
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                            {passwordErr && <p>{passwordErr}</p>}
+                        </Form.Group>
+                        <Row className="mt-3 justify-content-start">
+                            <Col sm="10" md="8" lg="6">
+                                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                                    Login         </Button>
+                            </Col></Row>
+                    </Form>
+                </Col></Row>
+        </Container>
     );
 }
 
@@ -75,5 +83,3 @@ LoginView.propTypes = {
     }),
     onLoggedIn: PropTypes.func.isRequired,
 };
-
-export default LoginView;
