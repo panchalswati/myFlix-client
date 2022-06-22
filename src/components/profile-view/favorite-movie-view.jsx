6b8cbeeb236e70478db0b430c311293deca815d8
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-22 17:15:48 +0200

    updated heroku app address

--- src/components/profile-view/favorite-movie-view.jsx
+++ src/components/profile-view/favorite-movie-view.jsx
@@ -15,8 +15,8 @@ export function FavouriteMoviesView(props) {
         return favouriteMoviesId.includes(m._id)
     })
 
-    const handleMovieDelete = (movieId) => {
-        axios.delete(`https://dashboard.heroku.com/apps/myflix-movies-heroku/users/${currentUser}/movies/${movies._Id}`, {
+    const handleMovieDelete = (moviesId) => {
+        axios.delete(` https://myflix-movies-heroku.herokuapp.com/${currentUser}/movies/${movies._Id}`, {
             headers: { Authorization: `Bearer ${token}` }
         })
             .then(() => {
@@ -31,23 +31,23 @@ export function FavouriteMoviesView(props) {
             {favouriteMoviesList.length === 0 ? (
                 <p>You have no favourite movies yet.</p>
             ) : (
-                favouriteMoviesList.map((movie) => {
+                favouriteMoviesList.map((movies) => {
                     return (
                         <Col xs={10} sm={8} md={6} lg={4} >
                             <Card id="movie-card">
-                                <Link to={`/movies/${movie._id}`}>
+                                <Link to={`/movies/${movies._id}`}>
                                     <Card.Img variant="top" src={movie.ImagePath} />
                                 </Link>
                                 <Card.Body>
-                                    <Card.Title>{movie.Title}</Card.Title>
-                                    <Card.Text>{movie.Description}</Card.Text>
-                                    <Link to={`/movies/${movie._id}`}>
+                                    <Card.Title>{movies.Title}</Card.Title>
+                                    <Card.Text>{movies.Description}</Card.Text>
+                                    <Link to={`/movies/${movies._id}`}>
                                         <Button className="button" variant="outline-primary" size="sm">Open</Button>
                                     </Link>
                                     <Button
                                         className="button ml-2"
                                         variant="outline-primary"
-                                        size="sm" onClick={() => { handleMovieDelete(movie._id) }} >
+                                        size="sm" onClick={() => { handleMovieDelete(movies._id) }} >
                                         Remove
                                     </Button>
                                 </Card.Body>
