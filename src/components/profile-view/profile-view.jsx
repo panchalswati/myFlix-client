f6d76beb7b74d5534863bd813cd66f4cac095101
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-22 17:16:18 +0200

    updated heroku app address

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import axios from 'axios';
 
 import { Button, Col, Container, Row } from 'react-bootstrap';
@@ -10,14 +9,14 @@ import { UpdateView } from './update-view';
 import './profile-view.scss';
 
 export function ProfileView(props) {
-    const [user, setUser] = useState(props.user);
+    const [users, setUser] = useState(props.user);
     const [movies, setMovies] = useState(props.movies);
     const [favouriteMovies, setFavouriteMovies] = useState([]);
     const currentUser = localStorage.getItem('user');
     const token = localStorage.getItem('token');
 
     const getUser = () => {
-        axios.get(`https://movime-api.herokuapp.com/users/${currentUser}`, {
+        axios.get(` https://myflix-movies-heroku.herokuapp.com/users/${currentUser}`, {
             headers: { Authorization: `Bearer ${token}` }
         })
             .then(response => {
@@ -32,11 +31,11 @@ export function ProfileView(props) {
     }, [])
 
     const handleDelete = () => {
-        axios.delete(`https://movime-api.herokuapp.com/users/${currentUser}`, {
+        axios.delete(` https://myflix-movies-heroku.herokuapp.com/users/${currentUser}`, {
             headers: { Authorization: `Bearer ${token}` }
         })
             .then(() => {
-                alert(`The account ${user.Username} was successfully deleted.`)
+                alert(`The account ${users.name} was successfully deleted.`)
                 localStorage.clear();
                 window.open('/register', '_self');
             }).
@@ -48,7 +47,7 @@ export function ProfileView(props) {
             <Row><h4>Your profile</h4></Row>
             <Row>
                 <Col className="label">name:</Col>
-                <Col className="value">{user.name}</Col>
+                <Col className="value">{users.name}</Col>
             </Row>
             <Row className="mt-3">
                 <Col className="label">Password:</Col>
@@ -56,11 +55,11 @@ export function ProfileView(props) {
             </Row>
             <Row className="mt-3">
                 <Col className="label">Email:</Col>
-                <Col className="value">{user.email}</Col>
+                <Col className="value">{users.email}</Col>
             </Row>
             <Row className="mt-3">
                 <Col className="label">Birthdate:</Col>
-                <Col className="value">{user.Birthdate}</Col>
+                <Col className="value">{users.Birthdate}</Col>
             </Row>
             <Row className="mt-5"><h4>Your favourite movies</h4></Row>
             <Row className="mt-3">
@@ -70,7 +69,7 @@ export function ProfileView(props) {
                     currentUser={currentUser}
                     token={token} />
             </Row>
-            <UpdateView user={user} />
+            <UpdateView users={users} />
             <Button className="d-block mt-5" variant="danger" onClick={handleDelete}>Delete profile</Button>
         </Container>
     )
