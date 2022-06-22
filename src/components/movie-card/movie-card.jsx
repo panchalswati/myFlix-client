ffc70af006bfb47cfa69f7ce8117c47209129f96
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-22 17:12:54 +0200

    added addToFavoriteList function

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -6,18 +6,35 @@ import './movie-card.scss';
 import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
+    addToFavoriteList(moviesId) {
+        const currentUser = localStorage.getItem('users');
+        const token = localStorage.getItem('token');
+        axios.put(`https://movie-api-93167.herokuapp.com/users/${currentUser}/movies/${moviesId}`,
+            {},
+            {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            .then((response) => {
+                console.log(response.data)
+                alert(`The movie was successfully add to your list.`)
+            }).
+            catch(error => console.error(error))
+    }
     render() {
-        const { movie } = this.props;
+        const { movies } = this.props;
 
         return (
-            <Card>
-                <Card.Img variant="top" src={movie.ImagePath} />
+            <Card id="movie-card">
+                <Link to={`/movies/${movies._id}`}>
+                    <Card.Img variant="top" src={movies.ImagePath} />
+                </Link>
                 <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
+                    <Card.Title>{movies.Title}</Card.Title>
+                    <Card.Text>{movies.Description}</Card.Text>
                     <Link to={`/movies/${movies._id}`}>
                         <Button variant="link">Open</Button>
                     </Link>
+                    <Button className="button ml-2" variant="outline-primary" size="sm" onClick={() => this.addToFavoriteList(movie._id)}>Add</Button>
                 </Card.Body>
             </Card>
         );
@@ -25,11 +42,18 @@ export class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string,
-        Description: PropTypes.string,
+    movies: PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Director: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string.isRequired,
+        }),
+        Genre: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        })
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
 };
-
-export default MovieCard;
