755dcdafa7b05c6c8fb4ef58aaa1de44a4585611
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-24 12:42:46 +0200

    added createStore ,Provider from 'redux'

--- src/index.jsx
+++ src/index.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 import { MainView } from './components/main-view/main-view';
+import createStore from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
 
-// Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp);
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <Container>
-                <MainView />
-            </Container>
+            <Provider store={store}>
+                <Container>
+                    <MainView />
+                </Container>
+            </Provider>
         );
     }
 }
+
+// Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
+
+// Tell React to render our app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
